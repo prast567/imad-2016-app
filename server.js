6659b931c40127f3be24869a9b30c5afea7bee63
prast567 <pk611886@gmail.com>
2016-09-30 01:55:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 app.get('/article_one',function(req,res)
 {
-    res.sendFile(path.join(__dirname,'ui','article.html'));
+    res.sendFile(path.join(__dirname,'ui','article_one.html'));
 });
 
 
